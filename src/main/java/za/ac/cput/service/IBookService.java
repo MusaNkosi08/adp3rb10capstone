861635ac1e6540d9be410775125962fca8695637
Ashton Mondreo Petersen (220219494) <ashtonpetersen0@gmail.com>
2025-05-24 22:32:21 +0200

    Added Service Package

--- src/main/java/za/ac/cput/service/IBookService.java
+++ src/main/java/za/ac/cput/service/IBookService.java
@@ -1,16 +1,17 @@
 /*
-IBookRepository.java
+IBookService.java
 Book Repository Interface
 Author: Ashton Mondreo Petersen (220219494)
  */
 
-package za.ac.cput.repository;
+package za.ac.cput.service;
 
 import za.ac.cput.domain.Book;
+import za.ac.cput.repository.IRepository;
 
 import java.util.List;
 
-public interface IBookRepository extends IRepository<Book, String> {
+public interface IBookService extends IRepository<Book, String> {
 List<Book> findByAuthor(String author, boolean displayOOS);
 List<Book> findByTitle(String title, boolean displayOOS);
 List<Book> findByGenre(String genre, boolean displayOOS);
