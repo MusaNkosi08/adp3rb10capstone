200d53e61cf4550bb5f6dee4851e7bd0fd50dc46
Ashton Mondreo Petersen (220219494) <ashtonpetersen0@gmail.com>
2025-05-25 20:30:21 +0200

    Controllers Added

--- src/main/java/za/ac/cput/controller/BookController.java
+++ src/main/java/za/ac/cput/controller/BookController.java
@@ -1,8 +1,39 @@
 package za.ac.cput.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Book;
+import za.ac.cput.domain.Order;
+import za.ac.cput.repository.IBookRepository;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/book")
 public class BookController {
 
+    @Autowired
+    private IBookRepository repository;
+
+    @PostMapping("/create")
+    public Book createBook(@RequestBody Book book) {
+        return this.repository.save(book);
+    }
+
+    @GetMapping("/{bookISBN}")
+    public Book getBook(@PathVariable String bookISBN) {
+
+        return repository.findById(bookISBN).orElse(null);
+    }
+
+    @PutMapping("/update")
+    public Book updateBook(@RequestBody Book book) {
+        return repository.save(book);
+    }
+
+    @DeleteMapping("/delete/{bookISBN}")
+    public void deleteBook(@PathVariable String bookISBN) {
+        repository.deleteById(bookISBN);
+    }
 }
+
--- src/main/java/za/ac/cput/service/impl/SupplyLineService.java
+++ src/main/java/za/ac/cput/service/impl/SupplyLineService.java
@@ -3,6 +3,7 @@ package za.ac.cput.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.domain.Book;
+import za.ac.cput.domain.SupplyLine;
 import za.ac.cput.repository.IBookRepository;
 import za.ac.cput.repository.ISupplyLineRepository;
 import za.ac.cput.service.IBookService;
@@ -11,42 +12,36 @@ import za.ac.cput.service.ISupplyLineService;
 import java.util.List;
 
 @Service
-public class SupplyLineService {
+public class SupplyLineService implements ISupplyLineService {
 
     @Autowired
     private static ISupplyLineService service;
     private static ISupplyLineRepository repository;
 
 
-    public static ISupplyLineService getService() {
-        if (service == null) {
-            service = new SupplyLineService();
-        }
-        return service;
-    }
 
 
     @Override
-    public Book create(Book book) {
-        return this.repository.save(book);
+    public SupplyLine create(SupplyLine line) {
+        return this.repository.save(line);
     }
 
     @Override
-    public Book read(String s) {
-        return this.repository.findById(s).orElse(null);
+    public SupplyLine read(Integer id) {
+        return this.repository.findById(id).orElse(null);
     }
 
     @Override
-    public Book update(Book book) {
-        return this.repository.save(book);
+    public SupplyLine update(SupplyLine line) {
+        return this.repository.save(line);
     }
 
     @Override
-    public void delete(String id) {
+    public void delete(Integer id) {
         this.repository.deleteById(id);
     }
     @Override
-    public List<Book> findAll() {
+    public List<SupplyLine> findAll() {
         return this.repository.findAll();
     }
 }
