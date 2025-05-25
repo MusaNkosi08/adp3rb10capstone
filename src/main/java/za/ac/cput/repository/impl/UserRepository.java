6b16daace58964b0dd3e22a398ceb9be43b53604
Aimee Paulus <222814969@mycput.ac.za>
2025-05-25 23:31:09 +0200

    User and role service updated

--- src/main/java/za/ac/cput/repository/impl/UserRepository.java
+++ src/main/java/za/ac/cput/repository/impl/UserRepository.java
@@ -5,11 +5,13 @@ package za.ac.cput.repository.impl;
   Date: 23 March 2025
  */
 
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.User;
 import za.ac.cput.repository.IUserRepository;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class UserRepository implements IUserRepository {
 
     private static UserRepository repository = null;
