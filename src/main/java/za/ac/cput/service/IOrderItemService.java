ce7307a40451676dcc47572efe38678fd36a5bad
Tyrese <Tyresentate25@gmail.com>
2025-08-22 22:12:44 +0200

    correct packaging

--- src/main/java/za/ac/cput/service/IOrderItemService.java
+++ src/main/java/za/ac/cput/service/IOrderItemService.java
@@ -1,4 +1,28 @@
 package za.ac.cput.service;
+import za.ac.cput.domain.OrderItem;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Author: TT Ntate (221817816)
+ */
 
 public interface IOrderItemService {
+    // CREATE
+    OrderItem create(OrderItem orderItem);
+
+    // READ (single)
+    Optional<OrderItem> read(int itemId);
+
+    // READ ALL
+    List<OrderItem> getAll();
+
+    // UPDATE
+    OrderItem update(int itemId, OrderItem orderItem);
+
+    // DELETE
+    void delete(int itemId);
 }
+
+
