973c4a5991813d557af0eb8cca772d80cd43566c
Tyrese <Tyresentate25@gmail.com>
2025-08-22 20:09:54 +0200

    correct packaging

--- src/main/java/za/ac/cput/repository/IOrderRepository.java
+++ src/main/java/za/ac/cput/repository/IOrderRepository.java
@@ -1,10 +1,9 @@
 package za.ac.cput.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.cput.domain.Order;
 import java.util.List;
 
-public interface IOrderRepository extends JpaRepository<Order, String> {
+public interface IOrderRepository extends IRepository<Order, String> {
 
     Order getOrder(int orderId);
 
@@ -16,5 +15,5 @@ public interface IOrderRepository extends JpaRepository<Order, String> {
 
     boolean updateOrder(Order order);
 
-    //boolean deleteOrder(int orderId); // Fixed typo: `DeleteOrder` → `deleteOrder`
+    boolean deleteOrder(int orderId); // Fixed typo: `DeleteOrder` → `deleteOrder`
 }
