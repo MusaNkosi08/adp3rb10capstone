1b18fa0fd23cdbc3a42a06c7d6bb6e7194f27171
AMP51 <129651659+AMP51@users.noreply.github.com>
2025-08-22 10:24:59 +0200

    Update SupplierController.java

--- src/main/java/za/ac/cput/controller/SupplierController.java
+++ src/main/java/za/ac/cput/controller/SupplierController.java
@@ -1,4 +1,36 @@
 package za.ac.cput.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Supplier;
+import za.ac.cput.repository.ISupplierRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/supplier")
 public class SupplierController {
+   @Autowired
+    private ISupplierRepository repository;
+
+    @PostMapping("/create")
+    public Supplier createSupplier(@RequestBody Supplier supplier) {
+        return this.repository.save(supplier);
+    }
+
+    @GetMapping("/{supplierID}")
+    public Book getSupplier(@PathVariable Long supplierID) {
+
+        return repository.findById(supplierID).orElse(null);
+    }
+
+    @PutMapping("/update")
+    public Supplier updateSupplier(@RequestBody Supplier supplier) {
+        return repository.save(supplier);
+    }
+
+    @DeleteMapping("/delete/{supplierID}")
+    public void deleteSupplier(@PathVariable Long supplierID) {
+        repository.deleteById(supplierID);
+    }
 }
