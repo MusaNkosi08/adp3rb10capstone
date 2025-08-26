66233765c930cbbb588cc0e0d2274cc795176dd0
AMP51 <129651659+AMP51@users.noreply.github.com>
2025-08-26 19:36:05 +0200

    Create BookCatalogueGUI.java

---
+++ src/main/java/za/ac/cput/views/BookCatalogueGUI.java
@@ -0,0 +1,14 @@
+package za.ac.cput.views;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class BookCatalogueGUI extends JFrame {
+
+  public BookCatalogueGUI(){
+
+  }
+
+  public
+}
