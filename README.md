cf8b0c66d5212140c107ab8062bac35f080404f2
Musa Banathi Nkosi <bmnkosi08@gmail.com>
2025-09-05 14:36:32 +0200

    ADP #3 working system

--- README.md
+++
@@ -1,2 +0,0 @@
-![ADP3 Capstone UML Diagram](https://github.com/AimeeLisa/adp3rb10capstone1/raw/master/ADP3_Capstone_UML.png)
-
--- src/main/java/za/ac/cput/repository/IOrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package za.ac.cput.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.domain.OrderItem;
-
-public interface IOrderItemRepository extends JpaRepository<OrderItem, Integer> {
-}
